ab5087201349acd13ccc86bc945f1f00b8487702
pertuzjonatha49-droid <pertuzjonatha49@gmail.com>
2026-08-10 07:34:28 -0500

    Add files via upload

---
+++ clase2.py
@@ -0,0 +1,15 @@
+# Area y perimetro de un rectangulo
+# Autor: estudiante UNICARIBE
+
+
+#1. definir dimensiones
+ancho = 5 
+alto = 6
+
+area = ancho + alto
+
+
+perimetro = 2 + (ancho + alto)
+
+print("area:", area)
+print("perimetro:", perimetro)
